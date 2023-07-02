653d3a26afc54d28ddca1d49d650c7896fe8b8b7
Rahul Shah <rahulnshah3012@gmail.com>
2023-07-02 13:45:37 -0400

    yes

--- src/main/java/Graphs/NumOfProvinces.java
+++ src/main/java/Graphs/NumOfProvinces.java
@@ -1,2 +1,74 @@
-package Graphs;public class NumOfProvinces {
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumOfProvinces {
+    public void bfs(int index, List<List<Integer>> adjList, boolean [] visited)
+    {
+        // add the starting point first to q, then while q is not empty, remove it, and loop all its neighbors and if they have not been visited, add them to the q and mark them visited
+        List<Integer> q = new ArrayList<>();
+        q.add(index);
+        visited[index] = true;
+
+        while(q.size() > 0)
+        {
+            int visitedInd = q.remove(0);
+            for(Integer i : adjList.get(visitedInd))
+            {
+                if(!visited[i])
+                {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        /*
+        This problem is basically asking us to find the number of connected components
+
+        We can use either bfs/dfs to traverse a whole component; and each connected component will have
+        a different starting point. A node that has not been visited from previous traversal, will become the starting point for its component.  We can keep a counter to increment whenever we find a node that has not been visited.
+
+        Approach:
+            Convert adjMatrix isConnected into a adjList; skip when i == j because for simplicity we assume a vertex can't be connected to itself
+            Loop visited array from 1 to n and when visited[i] == false, run bfs on i
+
+        Time:
+        Space:
+        */
+        List<List<Integer>> adjList = new ArrayList<>();
+        // Fill up arraylist with empty buckets just like u did in Top K freq elements problem
+        for(int i = 0; i < isConnected.length + 1; i++)
+        {
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < isConnected.length; i++)
+        {
+            for(int j = 0; j < isConnected[i].length; j++)
+            {
+                if(j != i && isConnected[i][j] == 1)
+                {
+                    // add j + 1 to i + 1's List
+                    adjList.get(i + 1).add(j + 1);
+                }
+            }
+        }
+        // we are given n vertices so the length of the visted array will be n + 1; indexed from 0 to n
+        boolean [] visited = new boolean[isConnected.length + 1];
+
+        int count = 0;
+
+        for(int i = 1; i < visited.length; i++)
+        {
+            if(!visited[i])
+            {
+                bfs(i, adjList, visited);
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/main/java/binarySearch/FindPeakElement.java
+++ src/main/java/binarySearch/FindPeakElement.java
@@ -1,2 +1,53 @@
-package binarySearch;public class FindPeakElement {
+package binarySearch;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        /*
+        Big assumptions in this problem:
+        num[0] is always bigger than nums before/outside of the nums list
+        last num is always bigger than the nums after/outside of the nums list
+        Adjacent nums are not equal
+
+        One approach (BF): You can treverse nums and see if a num is bigger than the pervious num and bigger than the num after it, if now continue searching.
+        This is an O(n) time soln
+
+        2nd approach: Apply binary search but only wen you know for sure that niether the very first or last elements are not peaks.
+        If they are just return one of them, else do bin search for the peak between index 1 and n - 2.
+        We also know that if nums has only one element that element itself is the only peak so return it
+
+        Time: 0(log n)
+        */
+        if(nums.length == 1){
+            return 0;
+        }
+        else if(nums[0] > nums[1])
+        {
+            return 0;
+        }
+        else if(nums[nums.length - 1] > nums[nums.length - 2])
+        {
+            return nums.length - 1;
+        }
+        else
+        {
+            int left = 1, right = nums.length - 2;
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+                if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+                {
+                    return mid; // we found our peak element so we return its index; the proble says it is garaunteed
+                }
+                else if(nums[mid - 1] > nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
 }
