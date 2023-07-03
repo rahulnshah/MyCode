d6d6c22b3ec69a8f62e268831b11278141e22e94
Rahul Shah <rahulnshah3012@gmail.com>
2023-07-03 16:01:59 -0400

    added kth problems

---
+++ src/main/java/kthProblems/KthLargestInAnArray.java
@@ -0,0 +1,26 @@
+package kthProblems;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargestInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        /* -Brute force:
+            - store all nums in a pq (min heap)
+            - while size of min heap > k, take out smallest elements (e.g. use poll)
+            - then return pq.poll()
+        */
+        Queue<Integer> minheap = new PriorityQueue<>();
+
+        for(int num : nums)
+        {
+            minheap.offer(num);
+        }
+
+        while(minheap.size() > k)
+        {
+            minheap.poll();
+        }
+        return minheap.poll();
+    }
+}
